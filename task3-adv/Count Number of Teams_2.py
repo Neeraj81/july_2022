34c61c7c1047f0150ba79de443be58245df30eb0
Neeraj <53859942+Neeraj81@users.noreply.github.com>
2022-07-28 22:21:39 +0530

    Update Count Number of Teams_2.py

--- task3-adv/Count Number of Teams_2.py
+++ task3-adv/Count Number of Teams_2.py
@@ -1,8 +1,8 @@
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
-        res = 0
+        result = 0
         for i in range(1, len(rating) - 1):
-            less, greater = [0, 0], [0, 0]
+            less, greater = [0]*2, [0]*2
             for j in range(i):
                 if rating[j] < rating[i]:
                     less[0] += 1
@@ -14,5 +14,5 @@ class Solution:
                     less[1] += 1
                 else:
                     greater[1] += 1
-            res += less[0] * less[1] + greater[0] * greater[1]
-        return res
+            result += less[0] * less[1] + greater[0] * greater[1]
+        return result
